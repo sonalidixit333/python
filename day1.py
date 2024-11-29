ef32680667b25ed3918317bfa10a91c53e27915d
sondixit <sonali.dixit@capgemini.com>
2024-11-29 18:38:55 +0530

    Day-1 python

---
+++ day1.py
@@ -0,0 +1,6 @@
+#printing the statement
+#Day 1 Project
+print("Welcome to the Band Name Generator\n")
+City = input("What is the name of your City ?? \n")
+Pet = input("What is the name of your Pet ?? \n")
+print("your Band name could be " + City +" "+Pet)
